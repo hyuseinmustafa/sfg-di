569fd593cd5f5152d7cc5f8b1a0154f42539e911
Hyusein Mustafa <hyseinmustafa@gmail.com>
2020-12-30 15:41:46 +0000

    Spring Profiles Exercise

---
+++ src/main/java/com/hyuseinmustafa/sfgdi/services/I18nSpanishGreetingService.java
@@ -0,0 +1,13 @@
+package com.hyuseinmustafa.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service("i18nService")
+@Profile("ES")
+public class I18nSpanishGreetingService implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Holla Mundo - ES";
+    }
+}
